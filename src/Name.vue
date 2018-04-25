b1a16bf98aef6c82259a291e7e85d30cd7930cc1
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-04-25 16:49:42 +0300

    simple clicker

---
+++ src/Name.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{total.toLocaleString()}}</h1>
+    <button @click="increment">+1</button>
+    <button @click="loop">loop</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        total: 0,
+      }
+    },
+    methods: {
+      increment: function () {
+        this.total += 1;
+      },
+      loop: function () {
+        setInterval(() => {
+          this.increment();
+        }, 1000);
+      },
+    },
+  }
+</script>
+
+<style>
+
+</style>
