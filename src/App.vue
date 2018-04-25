3e33e812ab82d79ea88fba5c36b8249064b11b0f
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-04-25 17:52:16 +0300

    added save for click

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div>
-    <name></name>
+    <h1>{{total.toLocaleString()}}</h1>
+    <button @click="increment(1)">+1</button>
+    <!--<button @click="loop(1)">loop</button>-->
   </div>
 </template>
 
 <script>
-  import name from './Name.vue'
-
   export default {
-    components: {
-      name,
+    data: () => {
+      return {
+        total: 0,
+      }
+    },
+    created: function () {
+      let dataStart = JSON.parse(localStorage.getItem('saveGame'));
+      this.total = dataStart !== null ? dataStart.total : 0;
+
+      // on close or refresh saveData
+      let dataSave = this.$data;
+      window.onbeforeunload = (e) => {
+        e.preventDefault();
+        localStorage.setItem('saveGame', JSON.stringify(dataSave));
+      };
+    },
+    methods: {
+      increment: function (count) {
+        this.total += count;
+      },
+      loop: function (count) {
+        setInterval(() => {
+          this.increment(count);
+        }, 1000);
+      },
     },
   }
 </script>
 
-<style lang="scss">
+<style>
 
 </style>
