f06d2aa11c35a3717a7ca58d5cce8915126f2b56
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-04-26 14:29:24 +0300

    boost and boost save added

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,42 @@
 <template>
   <div id="main-content">
-    <h1>{{total.toLocaleString()}}</h1>
-    <button @click="increment(1)">+1</button>
-    <button @click="loop(1)">loop</button>
+    <div id="total-count">{{total.toLocaleString()}}</div>
+    <button @click="increment(100)">+1</button>
+    <boostItems></boostItems>
   </div>
 </template>
 
 <script>
+  import boostItems from './BoostItems.vue';
+
   export default {
     data: () => {
       return {
         total: 0,
       }
     },
+    components: {
+      boostItems
+    },
     created: function () {
       // Get data from locale storage
       let dataStart = JSON.parse(localStorage.getItem('saveGame'));
       this.total = dataStart !== null ? dataStart.total : 0;
-
-      // on close or refresh saveData
-      let dataSave = this.$data;
-      window.onbeforeunload = (e) => {
-        e.preventDefault();
-        localStorage.setItem('saveGame', JSON.stringify(dataSave));
-      };
     },
     methods: {
       increment: function (count) {
         this.total += count;
       }, // this function will be used for looping elements
-      loop: function (count) {
+      loop: function (item) {
+        this.total -= parseInt(item.cost);
         setInterval(() => {
-          this.increment(count);
+          this.increment(item.boost);
         }, 1000);
+      },
+      startLoop: function (item) {
+        setInterval(() => {
+          this.increment(item.boost);
+        }, 1000 / parseInt(item.count));
       },
     },
   }
---
+++ src/BoostItems.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="boost-items">
+    <div class="boost-item" v-for="item in boostItems">
+      <span class="item-name">Имя: {{item.name}}</span>
+      <span class="item-count">Количество: <b>{{item.count}}</b></span>
+      <span class="item-cost">Стоимость: {{parseInt(item.cost)}}</span>
+      <span class="item-boost">Boost: {{item.boost}}</span>
+      <button class="item-boost-btn" @click="boost(item)" :disabled="parseInt(total) < parseInt(item.cost)">Click
+      </button>
+      <span></span>
+    </div>
+  </div>
+</template>
+
+<script>
+  import boostItems from './assets/json/boostItems.json'
+
+  export default {
+    data() {
+      // Get data from locale storage
+      return {
+        boostItems: {},
+      }
+    },
+    created: function () {
+      let dataStart = JSON.parse(localStorage.getItem('saveBoost'));
+      this.boostItems = dataStart !== null ? dataStart : boostItems;
+      if (dataStart !== null) {
+        for (let i in dataStart) {
+          if (dataStart[i].count !== 0) {
+            this.startBoost(dataStart[i]);
+          } else {
+            continue;
+          }
+        }
+      }
+      // on close or refresh saveData
+      let dataSave = this.$data.boostItems;
+      window.onbeforeunload = (e) => {
+        e.preventDefault();
+        localStorage.setItem('saveBoost', JSON.stringify(dataSave));
+        localStorage.setItem('saveGame', JSON.stringify({total: this.total}));
+      };
+    },
+    computed: {
+      total: function () {
+        // `this` указывает на экземпляр vm
+        return this.$parent.total
+      }
+    },
+    methods: {
+      boost: function (item) {
+        this.$parent.loop(item);
+        item.cost += parseInt(item.cost) * 0.05;
+        item.count++;
+      },
+      startBoost: function (item) {
+        this.$parent.startLoop(item);
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
